626b2df3e41d15cbc24af82a83faea0f4b95bcf5
OpokuNti10 <70270204+OpokuNti10@users.noreply.github.com>
2022-04-12 09:55:34 -0700

    Task 14 - advanced_functions_nested_loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - prints out the first 50 fibonacci numbers
+ * Return: Always return 0 (success\correct).
+ */
+
+int main(void)
+{
+	int i = 0;
+	long int a = 0, b = 1, next;
+
+	while (i < 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
