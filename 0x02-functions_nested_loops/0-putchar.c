0d095e791f84be2c63b4d9a93522abf5fe1ba604
OpokuNti10 <70270204+OpokuNti10@users.noreply.github.com>
2022-04-12 08:33:00 -0700

    Task 0 - Correction_2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,11 @@
-#include <main.h>
+#include <unistd.h>
 
 /**
  * _putcahr - writes the character c to stdout
  * @c: The character to print
  * Return: Always return 0 (success\correct)
  */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
