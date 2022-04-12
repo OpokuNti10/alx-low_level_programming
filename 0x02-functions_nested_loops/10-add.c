cccd0843406e5213b18087cf762c5371bfcbfd31
OpokuNti10 <70270204+OpokuNti10@users.noreply.github.com>
2022-04-12 09:14:43 -0700

    Task 10 - functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add -prints add two numbers
+*@i: print int i
+*@k: print int k
+* Return: Always return 0(success\correct).
+*/
+
+int add(int i, int k)
+{
+	return (i + k);
+}
